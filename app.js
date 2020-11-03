59b0b88e5a04cfbb2391382c6159efb782612c60
ErikVillarreal-bit <eriksamuel76@gmail.com>
2020-11-03 03:56:21 -0600

    crud finished

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+//Configuraciones
+
+//Configuracion de dotenv
 require('dotenv').config();
 
 //Configuracion para el depliegue de heroku
@@ -8,20 +12,22 @@ const port = process.env.PORT || 3000;
 
 //Conexion BD
 const mongoose = require('mongoose');
-
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@test.hpwm6.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{console.log('Base de datos conectada!!');})
 .catch((error)=>{console.log("Error de conexion: ",error);})
 
-//Configuraciones
-    //Configuracion de EJS, motor de plantillas
-    app.set("view engine", "ejs");
-    app.set("views", __dirname + "/views");
+//Configuracion de EJS, motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 //Middelwares
 app.use(express.static( __dirname + "/public"));
+
+//Configuracion de bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Rutas desde rutasWeb
 app.use('/', require('./router/rutasWeb'));
@@ -30,7 +36,7 @@ app.use('/mascotas', require('./router/mascotas'));
 //Middelware - En caso de no encontrarse un recurso
 app.use((req,res,next)=>{
     res.render("404",{
-        error:"Error 404!",
+        mensaje:"Error 404!",
         footer: "Footer error"
     });
 });
--- router/mascotas.js
+++ router/mascotas.js
@@ -1,6 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const Mascota = require('../models/mascota');
+//Rutas
+
+//R
+router.get('/crear',(req,res)=>{
+    try {
+        //const newMascota = new Mascota.create();
+        res.render('crearMascota',{
+            titulo: "Crear Mascota",
+            footer: "Footer de crear mascota"
+        });
+        
+    } catch (error) {
+        console.log("Error al mostrar la pagina de crear un elemento ", error);
+    }
+});
+
+router.get('/actualizar/:idActualizar',async(req,res)=>{
+    const id = req.params.idActualizar;
+    try {
+        const mascotaDB = await Mascota.findOne({_id: id});
+        res.render('editarMascota',{
+            titulo: "Editar Mascota",
+            data: mascotaDB,
+            footer: "Footer de editar mascota"
+        });
+        
+    } catch (error) {
+        console.log("Error al mostrar la pagina de editar un elemento ", error);
+    }
+})
 
 router.get('/', async(req,res)=>{
 
@@ -13,7 +43,82 @@ router.get('/', async(req,res)=>{
     });
 
     } catch (error) {
-        console.log("Error de consuta",error);
+        console.log("Error al mostrar todos los elementos ",error);
+    }
+});
+
+//R - leer un solo doc
+router.get('/:id', async(req,res)=>{
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findOne({_id: id});
+        res.render('detalleMascota',{
+            titulo:"Detalle",
+            footer: "Footer del detalle",
+            data: mascotaDB,
+            error: false
+        });
+    } catch (error) {
+        console.log("Error al mostar un solo elemento ",error);
+        res.render('detalleMascota',{
+            error: true,
+              titulo:"Error",
+            footer: "Footer del detalle",
+            mensaje:"No se encuentra el id"
+        });
+    }
+});
+
+//C
+router.post('/', async(req,res)=>{
+    const body = req.body;
+    try {
+        await Mascota.create(body);
+        res.redirect("/mascotas");
+    } catch (error) {
+        console.log("Error al crear el elemento ", error);
+    }
+});
+
+//D
+router.delete('/:idEliminar', async(req,res)=>{
+    const id = req.params.idEliminar;
+    try {
+        const mascotaDB = await Mascota.findByIdAndDelete({_id: id});
+        if(mascotaDB){
+            res.json({
+                error: false,
+                mensaje: "Eliminado!"
+            });
+        }else{
+            res.json({
+                error: true,
+                mensaje: "No se puede eliminar"
+            });
+        }
+    } catch (error) {
+        console.log("Error al eliminar el doc ",error);
+    }
+});
+
+//U
+router.put('/:idAct',async(req,res)=>{
+    const id = req.params.idAct;
+    const body = req.body;
+
+    try {
+        const mascotaDB = await Mascota.findByIdAndUpdate(id, body, { useFindAndModify: false});
+
+        res.json({
+            error: false,
+            mensaje: "Actualizado"
+        })
+    } catch (error) {
+        console.log("Error al actualizar los datos ", error);
+        res.json({
+            error: true,
+            mensaje: "No se puede actualizar"
+        })
     }
 });
 
